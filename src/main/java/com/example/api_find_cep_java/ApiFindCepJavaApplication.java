a03a6508a5a2bbb37445be8f54191519731d0a66
eduardo-gabriel-frainer <eduardogfrainer@gmail.com>
2025-02-05 15:59:57 -0300

    main commit

---
+++ src/main/java/com/example/api_find_cep_java/ApiFindCepJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_find_cep_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFindCepJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFindCepJavaApplication.class, args);
+	}
+
+}
