2a82dc6e8e062a83c7f15652d71ef170b1eac44e
eduardo-gabriel-frainer <eduardogfrainer@gmail.com>
2025-02-05 16:00:13 -0300

    test commit nao implementado

---
+++ src/test/java/com/example/api_find_cep_java/ApiFindCepJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_find_cep_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFindCepJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
